51a75b6da00f92236a5de26e0831df0fa83747c7
jang <dkjang@naver.com>
2020-08-06 21:17:34 +0900

    메인액티비티로 이동버튼

--- app/src/main/java/com/kakao/intent_20200806/FirstActivity.kt
+++ app/src/main/java/com/kakao/intent_20200806/FirstActivity.kt
@@ -1,11 +1,22 @@
 package com.kakao.intent_20200806
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        moveToMainBtn.setOnClickListener {
+//            MainActivity 이동
+            val myIntent = Intent(this,MainActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
     }
 }
