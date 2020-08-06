612e31db6e61cb2c6df1a89cfba82fea654f37b4
jang <dkjang@naver.com>
2020-08-06 22:21:36 +0900

    넥네임변경화면 완성

--- app/src/main/java/com/kakao/intent_20200806/EditNickNameActivity.kt
+++ app/src/main/java/com/kakao/intent_20200806/EditNickNameActivity.kt
@@ -2,10 +2,16 @@ package com.kakao.intent_20200806
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_nick_name)
+
+        okBtn.setOnClickListener {
+
+            finish()
+        }
     }
 }
