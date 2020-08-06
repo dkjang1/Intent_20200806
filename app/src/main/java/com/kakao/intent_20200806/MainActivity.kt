9674ab1777a95857f7a86a613ced82feebd7f0d6
jang <dkjang@naver.com>
2020-08-06 21:12:25 +0900

    Intent 활용 다른화면으로 이동

--- app/src/main/java/com/kakao/intent_20200806/MainActivity.kt
+++ app/src/main/java/com/kakao/intent_20200806/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.kakao.intent_20200806
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToFirstBtn.setOnClickListener {
+//            FirstActivity 이동
+            val myIntent = Intent(this,FirstActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
     }
 }
