966bf344cc38bebe046f8b86d2a6e44b07c28c5b
jang <dkjang@naver.com>
2020-08-10 19:38:43 +0900

    완료버튼 누르면 입력한 닉네임을 들고 finish()

--- app/src/main/java/com/kakao/intent_20200806/EditNickNameActivity.kt
+++ app/src/main/java/com/kakao/intent_20200806/EditNickNameActivity.kt
@@ -1,5 +1,7 @@
 package com.kakao.intent_20200806
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_nick_name.*
@@ -10,6 +12,17 @@ class EditNickNameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit_nick_name)
 
         okBtn.setOnClickListener {
+
+//            완료버튼-복귀:finish()
+//            백버튼을 눌러서 종료하는 것과 구별해야 함
+            val resultIntent = Intent()
+
+//           입력된 닉네임 : putExtra
+            val inputNickName = edtNickName.text.toString()
+            resultIntent.putExtra("newNickName", inputNickName)
+
+//            OK버튼을 눌렀다고 명시 + 데이터를 담고 있는 resultIntent
+            setResult(Activity.RESULT_OK, resultIntent)
 
             finish()
         }
